8e115319b50f58fef7d4ca889b78f79af2e31df8
David Horstmann <david.horstmann@arm.com>
2023-04-14 10:51:57 +0100

    Redirect roadmap.md to new roadmap location
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- conf.py
+++ conf.py
@@ -55,3 +55,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Tell reredirects to redirect moved pages
+redirects = {
+    "roadmap/index.html": "../project/roadmap/"
+}
